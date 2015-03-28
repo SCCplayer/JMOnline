a456ccaf9c67d87f76f2d58104e4f3d7fb458689
Kai <SCCplayer@gmx.de>
2015-03-28 20:14:50 +0100

    mit Mac-book geändert: ab jetzt läufts

--- src/gui/JMPlayer.java
+++ src/gui/JMPlayer.java
@@ -5,7 +5,7 @@ public class JMPlayer {
 	public static void main(String[] args) {
 		System.out.println("Weißt du was auch witzig ist, dass dein textfiles nen anderen Unicode verwenden --> öäü");
 		System.out.println("Ich möchte mal wieder festhalten das windows es verkackt hat");
-
+		System.out.println("ab jetzt nur noch UTF-8");
 	}
 
 }
