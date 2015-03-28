c9a40eb1c0b679c31d3ae26a9ecc46164fb27077
KaiDesktop <sccplayer@gmx.de>
2015-03-28 20:10:38 +0100

    Änderung von meinem Desktop Rechner aus

--- src/gui/JMPlayer.java
+++ src/gui/JMPlayer.java
@@ -4,6 +4,7 @@ public class JMPlayer {
 
 	public static void main(String[] args) {
 		System.out.println("Weißt du was auch witzig ist, dass dein textfiles nen anderen Unicode verwenden --> öäü");
+		System.out.println("Ich möchte mal wieder festhalten das windows es verkackt hat");
 
 	}
 
