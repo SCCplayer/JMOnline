ece4902312680657c114e594f88e088eb817719a
KaiDesktop <sccplayer@gmx.de>
2015-03-28 20:02:39 +0100

    ab jetzt nur noch utf-8 bitte ;)

--- src/gui/JMPlayer.java
+++ src/gui/JMPlayer.java
@@ -3,11 +3,6 @@ package gui;
 public class JMPlayer {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println("zweite änderung");
-		System.out.println("�nderung Flo :D");
-		System.out.println("Noch eine �nderung");
-		System.out.println("Und noch eine �nderung");
 		System.out.println("Weißt du was auch witzig ist, dass dein textfiles nen anderen Unicode verwenden --> öäü");
 
 	}
