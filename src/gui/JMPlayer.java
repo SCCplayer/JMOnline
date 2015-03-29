ca37541bbc2198d3c90d5cbfb1b5e0dc073998d4
Kai <SCCplayer@gmx.de>
2015-03-29 12:10:46 +0200

    nach einem fetch lade ich wieder sachen hoch

--- src/gui/JMPlayer.java
+++ src/gui/JMPlayer.java
@@ -6,6 +6,7 @@ public class JMPlayer {
 		System.out.println("Weißt du was auch witzig ist, dass dein textfiles nen anderen Unicode verwenden --> öäü");
 		System.out.println("Ich möchte mal wieder festhalten das windows es verkackt hat");
 		System.out.println("ab jetzt nur noch UTF-8");
+		System.out.println("Nach einem fetch lade ich wieder sachen hoch");
 	}
 
 }
