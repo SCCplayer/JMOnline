ff87704c2f624a905edde62b6c6a04eac28fb190
Kai <SCCplayer@gmx.de>
2015-03-30 09:45:25 +0200

    Änderung nach dem Flo nicht pullen konnte

--- src/gui/JMPlayer.java
+++ src/gui/JMPlayer.java
@@ -7,6 +7,7 @@ public class JMPlayer {
 		System.out.println("Ich möchte mal wieder festhalten das windows es verkackt hat");
 		System.out.println("ab jetzt nur noch UTF-8");
 		System.out.println("Nach einem fetch lade ich wieder sachen hoch");
+		System.out.println("Kann ich noch Änderungen machen?");
 	}
 
 }
