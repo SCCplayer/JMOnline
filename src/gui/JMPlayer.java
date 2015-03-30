b3c9584462c7b462a0198b8160ea7036e51f177d
Kai <SCCplayer@gmx.de>
2015-03-30 16:52:25 +0200

    Ich hab die Hosen an

--- src/gui/JMPlayer.java
+++ src/gui/JMPlayer.java
@@ -3,12 +3,6 @@ package gui;
 public class JMPlayer {
 
 	public static void main(String[] args) {
-		System.out.println("Weißt du was auch witzig ist, dass dein textfiles nen anderen Unicode verwenden --> öäü");
-		System.out.println("Ich möchte mal wieder festhalten das windows es verkackt hat");
-		System.out.println("ab jetzt nur noch UTF-8");
-		System.out.println("Nach einem fetch lade ich wieder sachen hoch");
-		System.out.println("Kann ich noch Änderungen machen?");
-		System.out.println("Flo hats wieder kaputt gemacht");
+		System.out.println("Flo hat gar nichts zu sagen");
 	}
-
 }
